d9913f0cfed0e3f8121066c54a02bd60a2f719bf
manan-jain09 <mananjain0209@gmail.com>
2020-10-20 16:46:18 +0530

    Vanished the Piugs

--- sketch.js
+++ sketch.js
@@ -71,7 +71,12 @@ function mouseDragged(){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
 
-
 function mouseReleased(){
     slingshot.fly();
+}
+function keyPressed() {
+    if (keyCode === 32) {
+        bird = new Bird(200,50);
+        slingshot.attach(bird.body);
+    }
 }
